75c131c239019939329ea0c464c2c459e0e1f9a4
ljubobratovicrelja <ljubobratovic.relja@gmail.com>
2017-05-19 20:13:27 +0200

    Redesign of the linear interpolation.

--- source/dcv/imgproc/interpolate.d
+++ source/dcv/imgproc/interpolate.d
@@ -60,19 +60,22 @@ Params:
 Returns:
     Interpolated resulting value.
 */
-pure T linear(SliceKind kind, size_t[] packs, T, P, size_t N)(Slice!(kind, packs, T*) slice, P[N] pos...)
-    if (packs.length == 1)
+pure auto linear(SliceKind kind, size_t[] packs, Iterator, P, size_t N)(Slice!(kind, packs, Iterator) slice, P[N] pos...)
 {
-    // TODO: document
-    static assert(N == packs[0], "Interpolation indexing has to be of same dimension as the input slice.");
+    static assert(packs.length == 1, "Packed slices are not supported.");
+    static assert(packs[0] == N, "Invalid indexing dimensionality.");
 
-    static if (pos.length == 1)
+    static if (N == 1)
     {
         return linearImpl_1(slice, pos[0]);
     }
-    else static if (pos.length == 2)
+    else static if (N == 2)
     {
         return linearImpl_2(slice, pos[0], pos[1]);
+    }
+    else static if (N == 3)
+    {
+        return linearImpl_3(slice, pos[0], pos[1], pos[2]);
     }
     else
     {
@@ -105,11 +108,12 @@ unittest
 
 private:
 
-pure @fastmath auto linearImpl_1(T)(Slice!(SliceKind.contiguous, [1], T*) range, double pos)
+pure @fastmath auto linearImpl_1(Iterator)(Slice!(Contiguous, [1], Iterator) range, float pos)
 {
     import mir.math.internal;
 
     assert(pos < range.length);
+    alias T = DeepElementType!(typeof(range));
 
     if (pos == range.length - 1)
     {
@@ -117,13 +121,13 @@ pure @fastmath auto linearImpl_1(T)(Slice!(SliceKind.contiguous, [1], T*) range,
     }
 
     size_t round = cast(size_t)pos.floor;
-    double weight = pos - cast(double)round;
+    float weight = pos - cast(float)round;
 
     static if (isIntegral!T)
     {
         // TODO: is this branch really necessary?
-        auto v1 = cast(double)range[round];
-        auto v2 = cast(double)range[round + 1];
+        auto v1 = cast(float)range[round];
+        auto v2 = cast(float)range[round + 1];
     }
     else
     {
@@ -133,17 +137,19 @@ pure @fastmath auto linearImpl_1(T)(Slice!(SliceKind.contiguous, [1], T*) range,
     return cast(T)(v1 * (1. - weight) + v2 * (weight));
 }
 
-pure @fastmath auto linearImpl_2(SliceKind kind, size_t[] packs, T)(Slice!(kind, packs, T*) range, double pos_x, double pos_y)
+pure @fastmath auto linearImpl_2(SliceKind kind, size_t[] packs, Iterator)(Slice!(kind, packs, Iterator) range, float pos_x, float pos_y)
 {
     import mir.math.internal : floor;
     import std.traits : Unqual;
+
+    alias T = Unqual!(DeepElementType!(typeof(range)));
 
     assert(pos_x < range.length!0 && pos_y < range.length!1);
 
     size_t rx = cast(size_t)pos_x.floor;
     size_t ry = cast(size_t)pos_y.floor;
-    double wx = pos_x - cast(double)rx;
-    double wy = pos_y - cast(double)ry;
+    float wx = pos_x - cast(float)rx;
+    float wy = pos_y - cast(float)ry;
 
     auto w00 = (1. - wx) * (1. - wy);
     auto w01 = (wx) * (1. - wy);
@@ -156,11 +162,11 @@ pure @fastmath auto linearImpl_2(SliceKind kind, size_t[] packs, T)(Slice!(kind,
     static if (isIntegral!T)
     {
         // TODO: (same as in 1D vesion) is this branch really necessary?
-        double v1, v2, v3, v4;
-        v1 = cast(double)range[rx, ry];
-        v2 = cast(double)range[x_end ? rx : rx + 1, ry];
-        v3 = cast(double)range[rx, y_end ? ry : ry + 1];
-        v4 = cast(double)range[x_end ? rx : rx + 1, y_end ? ry : ry + 1];
+        float v1, v2, v3, v4;
+        v1 = cast(float)range[rx, ry];
+        v2 = cast(float)range[x_end ? rx : rx + 1, ry];
+        v3 = cast(float)range[rx, y_end ? ry : ry + 1];
+        v4 = cast(float)range[x_end ? rx : rx + 1, y_end ? ry : ry + 1];
     }
     else
     {
@@ -172,3 +178,44 @@ pure @fastmath auto linearImpl_2(SliceKind kind, size_t[] packs, T)(Slice!(kind,
     return cast(T)(v1 * w00 + v2 * w01 + v3 * w10 + v4 * w11);
 }
 
+pure @fastmath auto linearImpl_3(SliceKind kind, size_t[] packs, Iterator)(Slice!(kind, packs, Iterator) range, float pos_x, float pos_y, float pos_z)
+{
+    import mir.math.internal : floor;
+    import std.traits : Unqual;
+
+    alias T = Unqual!(DeepElementType!(typeof(range)));
+
+    assert(pos_x < range.length!0 && pos_y < range.length!1);
+
+    size_t channel = cast(size_t)pos_z;
+    size_t rx = cast(size_t)pos_x.floor;
+    size_t ry = cast(size_t)pos_y.floor;
+    float wx = pos_x - cast(float)rx;
+    float wy = pos_y - cast(float)ry;
+
+    auto w00 = (1. - wx) * (1. - wy);
+    auto w01 = (wx) * (1. - wy);
+    auto w10 = (1. - wx) * (wy);
+    auto w11 = (wx) * (wy);
+
+    auto x_end = rx == range.length!0 - 1;
+    auto y_end = ry == range.length!1 - 1;
+
+    static if (isIntegral!T)
+    {
+        // TODO: (same as in 1D vesion) is this branch really necessary?
+        float v1, v2, v3, v4;
+        v1 = cast(float)range[rx, ry, channel];
+        v2 = cast(float)range[x_end ? rx : rx + 1, ry, channel];
+        v3 = cast(float)range[rx, y_end ? ry : ry + 1, channel];
+        v4 = cast(float)range[x_end ? rx : rx + 1, y_end ? ry : ry + 1, channel];
+    }
+    else
+    {
+        auto v1 = range[rx, ry, channel];
+        auto v2 = range[x_end ? rx : rx + 1, ry, channel];
+        auto v3 = range[rx, y_end ? ry : ry + 1, channel];
+        auto v4 = range[x_end ? rx : rx + 1, y_end ? ry : ry + 1, channel];
+    }
+    return cast(T)(v1 * w00 + v2 * w01 + v3 * w10 + v4 * w11);
+}
